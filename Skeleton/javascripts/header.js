9a8c82f334dc90f93ec6869e44fbae25a657bb32
Philip Cole <philip@pcole.me>
2013-02-08 13:56:06 +0000

    Layered header backgrounds

--- Skeleton/javascripts/header.js
+++ Skeleton/javascripts/header.js
@@ -1,16 +1,32 @@
  $(document).ready(function(){
 
 	var $header = $("#header");
+	var $backBg = $("#header-back-bg")
+	var $middleBg = $("#header-middle-bg")
 	var $page = $('#page-container');
+
+	backSpeed = 0.6
+	middleSpeed = 0.8
+	scrollTop = 0
+	backBgPos = 0
+	middleBgPoss = 198
 
 	$(document).scroll(function(e){
 
-		if($(this).scrollTop() > 280){
+		newScrollTop = $(this).scrollTop()
+		d = newScrollTop - scrollTop
+
+		if(newScrollTop > 280){
 			$page.css({"margin-top":"476px"});
 			$header.addClass("fixed");
     }else{
     	$page.css({"margin-top":"0px"});
     	$header.removeClass("fixed");
+
+    	backBgPos = backBgPos + backSpeed/d
+    	/*$backBg.css({
+    		"background-position": "0px " + backBgPos + "px"
+    	});*/
     }
   });
  });
