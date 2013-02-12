f7353b78ee7bb91b912c3f9e34df9851e7ea4a48
Michael Hobson <hobson.michael@gmail.com>
2013-02-12 15:44:33 +0000

    - More changes to contact page - Added blog theme

---
+++ WPTheme/footer.php
@@ -0,0 +1,52 @@
+<?php st_before_footer();?>
+
+<?php wp_footer();?>
+
+<?php st_after_footer();?>
+
+<!-- BEGIN FOOTER -->
+	<div class="container footer">
+		
+		<div class="social">
+			<div id="tweetbtn" class="nomobile">
+				<a href="https://twitter.com/share" class="twitter-share-button" data-url="http://www.meetstrange.com" data-text="Inusitus Bonum Est" data-related="meetstrange">Tweet</a>
+			</div>
+			<div id="fblike" class="nomobile">
+				<fb:like href="https://www.facebook.com/meetstrange" send="false" layout="button_count" width="80" show_faces="false"></fb:like>
+			</div>
+		</div>
+
+		<img class="eggs" src="images/footer-eggs.png" alt="Eggs"/>	
+		<img class="mound" src="images/footer-ground.png" alt="Mound" class="scale-with-grid"/>	
+
+	</div>
+	<!-- END FOOTER -->
+
+
+	<!-- JS
+	================================================== -->
+	<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
+	<script src="http://connect.facebook.net/en_US/all.js#xfbml=1"></script>
+	<script src="javascripts/header.js" type="text/javascript" /></script>
+	
+	
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-37834457-1']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>	
+
+
+<!-- End Document
+================================================== -->
+</body>
+</html>
